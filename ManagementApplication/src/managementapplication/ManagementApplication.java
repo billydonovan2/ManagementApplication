9ee2398b27038774d11b35a701abfd29c7c37d68
billydonovan2 <billy.donovan@comcast.net>
2017-11-05 17:55:53 -0500

    Sample Class

--- ManagementApplication/src/managementapplication/ManagementApplication.java
+++ ManagementApplication/src/managementapplication/ManagementApplication.java
@@ -15,7 +15,10 @@ public class ManagementApplication {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Hello");
+       System.out.println("Hello"); 
+       System.out.println("Hi");
+       
+    
     }
     
 }
